98bb66f7781f5e04d8c63c298713403a2e09980b
chandrakala <chadrakalasv1502@gmail.com>
2022-04-26 14:52:54 +0530

    [chandu]sort

---
+++ array.sh
@@ -0,0 +1,37 @@
+#!/bin/bash 
+read -p "Enter the value for a: " a
+read -p "Enter the value for b: " b
+read -p "Enter the value for c: " c
+declare -A comp
+for (( i=0; i<4; i++ ))
+do
+  case $i in
+    0)
+  	comp[com$i]=$((a+(b*c)))
+  	echo comp[com$i]
+	;;
+    1)
+  	comp[com$i]=$(((a*b)+c))
+  	echo comp[com$i]
+	;;
+    2)
+  	comp[com$i]=$((c+(a/b)))
+  	echo comp[com$i]
+	;;
+    3)
+  	comp[com$i]=$(((a%b)+c))
+  	echo comp[com$i]
+	;;
+  esac
+done
+echo Dictionary : ${comp[@]}
+echo Dict_keys : ${!comp[@]}
+l=${#comp[@]}
+echo Length of dictionary: $l
+
+for (( i=0; i<$l; i++ ))
+do
+    arr[$i]=$((comp[com$i]))
+done
+echo Array : ${arr[@]}
+echo Arr_indexes : ${!arr[@]}
